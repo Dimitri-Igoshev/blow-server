a1287d61d40870203bd40ecf81fe222c85575778
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2025-04-15 11:42:24 +0300

    get ip

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Ip } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Get('get-ip')
+  async myEndpointFunc(@Ip() ip) {
+    return { ip };
+  }
+
+  @Post()
+  create(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.create(createAuthDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+    return this.authService.update(+id, updateAuthDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }
+}
