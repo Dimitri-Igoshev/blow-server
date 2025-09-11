a844904651cb7640564b64bf3db4eb9e294ad410
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-09-11 17:45:57 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,6 @@ async function bootstrap() {
 
   app.set('trust proxy', true); // теперь TypeScript не ругается
 
-  const port = Number(process.env.PORT) || 8080;
-  await app.listen(port, '0.0.0.0');
-
-  // await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
